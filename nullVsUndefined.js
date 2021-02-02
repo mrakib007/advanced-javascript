23f3f070280bd2a83197d0e27a4cffb2d6811e68
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-02 07:03:57 +0600

    null vs undefined cleared

---
+++ nullVsUndefined.js
@@ -0,0 +1,21 @@
+// // let pakhi;
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return;
+// }
+// function add(num1,num2=10){
+//     console.log(num1, num2);
+// }
+// const result = add(13,82);
+// const result = add(13);
+// console.log(result);
+// console.log(pakhi);
+
+// const premik = {name:'smart dude',phone:'55456'};
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun); //falsy
+
+let ages = [2,5,9];
+console.log(ages[11]);
